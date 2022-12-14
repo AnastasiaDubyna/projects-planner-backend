287aa0a001866e37464bf165ac00051f01ce20e5
Anastasiia Dubyna <anastasiia.dubyna@dataart.com>
2022-12-14 11:31:52 +0100

    get endpoint added

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const tasksGetter = require('./routes/tasksGetter');
 const app = express();
+const cors = require('cors');
 
-
+app.use(cors({origin: "http://localhost:3000"}));
 app.use('/getTasks', tasksGetter);
 
 app.get('/', (req, res) => { 
